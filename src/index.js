0c5a9a49092303177481b1d04f59ec2f4734bf00
SvetlankaKuzyaka <svetlanka-kuznecov@yandex.ru>
2019-03-20 15:48:50 +0300

    add solve

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency <= 0) return {};
+    if (currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    var coins = {"H":50, "Q":25, "D":10, "N":5, "P":1};
+    var buf_obj = {};
+    var buf_cur = currency;
+    for (key in coins) {
+      var buf = Math.floor(buf_cur/coins[key]);
+      if (buf != 0) buf_obj[key] = buf;
+      buf_cur = buf_cur % coins[key];
+    }
+    return buf_obj;
 }
